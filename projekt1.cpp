72d68bfa724b39581ee27af0b24119ba2017a981
Kacper Nowakowski <kacpernowakowski13@gmail.com>
2024-06-27 20:25:43 +0200

    Add factorial function to projekt1.cpp

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+int factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    else return n * factorial(n - 1);
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
