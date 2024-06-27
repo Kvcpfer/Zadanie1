7c13bdb1459ffe8ac3debab83aaaf8f86af66a53
Kacper Nowakowski <kacpernowakowski13@gmail.com>
2024-06-27 20:28:07 +0200

    Add menu usage of prime check function

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+int factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    else return n * factorial(n - 1);
+}
+
 bool isPrime(int n) {
     if (n <= 1) return false;
     for (int i = 2; i < n; i++) {
@@ -11,9 +16,9 @@ bool isPrime(int n) {
 }
 
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
     int wyjscie;
     do {
         cout << endl;
@@ -21,7 +26,20 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Oblicz silnie" << endl;
+        cout << "2. Sprawdz czy liczba jest pierwsza" << endl;
         cin >> wyjscie;
+
+        if (wyjscie == 1) {
+            cout << "Silnia z " << a << " wynosi " << factorial(a) << endl;
+        } else if (wyjscie == 2) {
+            if (isPrime(a)) {
+                cout << a << " jest liczba pierwsza" << endl;
+            } else {
+                cout << a << " nie jest liczba pierwsza" << endl;
+            }
+        }
+
     } while(wyjscie != 0);
     return 0;
 }
